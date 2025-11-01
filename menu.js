58957879b92d6bf1bf19cfad71e7f93b6260e3de
JOAO PEDRO CALADO DE OLIVEIRA <joao.pcoliveira21@senacsp.edu.br>
2025-11-01 11:05:45 -0300

    finalizado menu de cadastro do sena com POO, incluindo cadastro de professor e alunos

--- menu.js
+++ menu.js
@@ -1,69 +1,121 @@
 // instalada a biblioteca que permite entrada de dados
-import PromptSync, { } from "prompt-sync";
+import PromptSync from "prompt-sync";
 
 // aciona a função da biblioteca
 let prompt = PromptSync();
 
 // class é um modelo de um objeto
 class Pessoa {
+  //atributo privado
+  #senha;
 
+  //construtor constroi mais de um objeto
+  //para por os valores de cada objeto colocamos via parametro
+  constructor(nome, senha) {
+    this.nome = nome;
+    this.#senha = senha;
+  }
 
-    //atributo privado
-    #senha;
+  apresentar() {
+    console.log(`Olá me chamo ${this.nome}, tenho ${this.idade} anos`);
+  }
 
-    //construtor constroi mais de um objeto
-    //para por os valores de cada objeto colocamos via parametro
-    constructor(nome, idade, dataNasc, senha) {
-        this.nome = nome;
-        this.idade = idade;
-        this.dataNasc = dataNasc;
-        this.#senha = senha;
+  get senha() {
+    let verSenha = prompt("Digite 1 se quiser ver sua senha: ");
+
+    if (verSenha == 1) {
+      let perguntaSenha = prompt("Qual sua data de nascimento? ");
+
+      if (perguntaSenha == dataNascPessoa) {
+        console.log(`Sua senha é ${this.#senha}`);
+      } else {
+        console.log("Data de nascimento incorreta");
+      }
     }
-
-    apresentar() {
-        console.log(`Olá me chamo ${this.nome}, tenho ${this.idade} anos`)
-    }
-
-    get senha() {
-        let verSenha = prompt("Digite 1 se quiser ver sua senha: ")
-
-        if (verSenha == 1) {
-
-            let perguntaSenha = prompt("Qual sua data de nascimento? ")
-
-            if (perguntaSenha == dataNascPessoa) {
-                console.log(`Sua senha é ${this.#senha}`)
-            } else {
-                console.log("Data de nascimento incorreta")
-            }
-        }
-
-    }
-
+  }
 }
 
-let nomePessoa = prompt("Digite o seu nome: ");
-let idadePessoa = parseInt(prompt("Digite a sua idade: "));
-let dataNascPessoa = prompt("Digite sua data de nascimento no formato americano(ano-mês-dia): ")
-let senhaPessoa = prompt("Digite sua senha: ")
+//Class aluno
+class Aluno extends Pessoa {
+  constructor(nome, senha, RA) {
+    super(nome, senha);
+    this.RA = RA;
+  }
 
+  //polimorfismo override
+  apresentar() {
+    console.log(`Olá sou o aluno ${this.nome} e meu RA é ${this.RA}`);
+  }
+}
 
+//Class professor
+class Professor extends Pessoa {
+  //os parametros sao os valores das chaves(propriedades)
+  constructor(nome, senha, salario, registro) {
+    super(nome, senha);
 
-//criando o objeto
-const novaPessoa = new Pessoa(
-    nomePessoa,
-    idadePessoa,
-    dataNascPessoa,
-    senhaPessoa
+    //sao as chaves = propriedades da classe
+    this.salario = salario;
+    this.registro = registro;
+  }
+
+  apresentar() {
+    console.log(
+      `Olá sou o professor ${this.nome}, meu registro é ${this.registro} e meu salario é ${this.salario}`
+    );
+  }
+}
+
+// pergunte ao usuario se ele e professor ou aluno
+// se for professor digite 1 e alem das perguntas que a class pessoa ja tem, faca perguntas que sao da suas proprias caracteristicas
+// se for aluno digite 2 e alem das perguntas que a class pessoa ja tem, faca perguntas que sao da suas proprias caracteristicas
+
+let alunoOuProfessor = parseInt(
+  prompt("Digite 1 para professor ou 2 para aluno")
 );
 
+let nomePessoa = prompt("Digite seu nome: ");
+let senhaPessoa = prompt("Digite sua senha: ");
 
-//aciona a função do objeto ja criado
-novaPessoa.apresentar();
+let pessoa;
 
+if (alunoOuProfessor == 1) {
+  let salarioProfessor = parseFloat(prompt("Digite seu salario: "));
+  let registroProfessor = prompt("Digite seu registro: ");
 
+  pessoa = new Professor(
+    nomePessoa,
+    senhaPessoa,
+    salarioProfessor,
+    registroProfessor
+  );
+  console.log(pessoa);
+} else if (alunoOuProfessor == 2) {
+  let raAluno = prompt("Informe seu RA: ");
 
-//exibir todo o objeto
-console.log(novaPessoa);
+  pessoa = new Aluno(nomePessoa, senhaPessoa, raAluno);
+  console.log(pessoa);
+} else {
+  console.log("Digite um numero valido");
+}
 
-novaPessoa.senha;
+// let nomePessoa = prompt("Digite o seu nome: ");
+// let idadePessoa = parseInt(prompt("Digite a sua idade: "));
+// let dataNascPessoa = prompt("Digite sua data de nascimento no formato americano(ano-mês-dia): ")
+// let senhaPessoa = prompt("Digite sua senha: ")
+
+// //criando o objeto (instanciando)
+// const novaPessoa = new Pessoa(
+//     nomePessoa,
+//     idadePessoa,
+//     dataNascPessoa,
+//     senhaPessoa
+// );
+
+// //aciona a função do objeto ja criado
+pessoa.apresentar();
+
+// //exibir todo o objeto
+// console.log(novaPessoa);
+
+// novaPessoa.senha;
